bfa26d919f8585698c235805caaf16ac1cdda0f4
Nittur <nittur.pavan@gmail.com>
2023-03-12 23:45:18 +0530

    Jupiter weight calculator CLI

--- jupiter_calc/src/main.rs
+++ jupiter_calc/src/main.rs
@@ -1,8 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-    calculate_weight_on_jupiter(65.0);
-}
+use std::io;
 
+fn main() {
+    println!("Enter the weight on earth (kg): ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    //remove white spaces with trim and unwarp to handle fails
+    let weight: f32 = input.trim().parse().unwrap();
+    dbg!(weight);
+
+    println!("Weight on earth (kg): {}", weight);
+    let jupiter_weight = calculate_weight_on_jupiter(weight);
+    println!("Weight on Jupiter : {}kg",jupiter_weight);
+}
+  
 fn calculate_weight_on_jupiter(weight: f32) -> f32 {
-    200.0
+    (weight/9.81)*24.79
 }
